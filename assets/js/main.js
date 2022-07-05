c38624e46defc571f173e94cd572c54a496e2b01
Filippo Floramo <filifloramo@gmail.com>
2022-07-05 14:16:30 +0200

    moved all the scripts into a single file

--- assets/js/main.js
+++ assets/js/main.js
@@ -35,6 +35,42 @@ minus.appendChild(minusBtn);
 
 
 
+/* Creating Handling Functions */
+
+let count = 0;
+
+function increment() {
+
+    count++;
+    countEl.textContent = count;
+}
+
+function decrement() {
+
+    if (count > 0) count--;
+    
+    countEl.textContent = count;
+}
+
+function resetSave() {
+    let results = "Previous count: " + count;
+
+    score.textContent = results;
+
+    count = 0;
+    countEl.textContent = count;
+} 
+
+
+
+
+plus.addEventListener("click", increment);
+minus.addEventListener("click", decrement);
+reset.addEventListener("click", resetSave);
+
+
+
+
 
 
 
